37e4d9961340c4b16b47e3c213180e929ecbdd1a
oysteinbarstadLaptop <oebars@gmail.com>
2018-01-22 11:40:00 +0100

    initial layout

---
+++ RusseSamfunnet/Templates/navigation.php
@@ -0,0 +1,6 @@
+<a href="feed.php">Feed</a>
+<a href="knots.php">Knuter</a>
+<a href="scoreboard.php">Scoreboard</a>
+<a href="community.php">Samfunn</a>
+<a href="userprofile.php">Brukerprofil</a>
+<a href="#">Log ut</a>
