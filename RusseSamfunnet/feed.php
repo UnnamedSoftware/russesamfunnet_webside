ba02936b532f56667cc03c2aa20cd9357dcd31e7
Kristian <krihus05@gmail.com>
2018-05-22 08:32:16 +0200

    bare en test

--- RusseSamfunnet/feed.php
+++ RusseSamfunnet/feed.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Russesamfunnet</title>
+        <title>Russesamfunnet - Feed</title>
         <link rel="stylesheet" type="text/css" href="CSS/normalize.css">
         
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
